7fe6152f4ae9bd17361c5f4c446ca858c178f5c3
sally7788 <0702sally@gmail.com>
2025-07-17 10:45:16 +0900

    conn  업로드

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -2,7 +2,7 @@ import pendulum
 
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.sdk import DAG, chain
-# from airflow.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="dags_conn_test",
@@ -11,28 +11,28 @@ with DAG(
     catchup=False, #누락되었던 구간을 돌릴 것인지? 1/1에 올리고 시작을 3/1에 한다면 그 두달을 돌릴 것인지
     tags=["example", "example2", "example3"],
 ) as dag: 
-    t1 = BashOperator(
+    t1 = EmptyOperator(
         task_id="t1")
     
-    t2 = BashOperator(
+    t2 = EmptyOperator(
         task_id="t2")
     
-    t3 = BashOperator(
+    t3 = EmptyOperator(
         task_id="t3")
     
-    t4 = BashOperator(
+    t4 = EmptyOperator(
         task_id="t4")
     
-    t5 = BashOperator(
+    t5 = EmptyOperator(
         task_id="t5")
     
-    t6 = BashOperator(
+    t6 = EmptyOperator(
         task_id="t6")
     
-    t7 = BashOperator(
+    t7 = EmptyOperator(
         task_id="t7")
     
-    t8 = BashOperator(  
+    t8 = EmptyOperator(  
         task_id="t8")
     
     
