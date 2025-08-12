bc87abbcb4e9445eea7eb9faef44f076afc83815
sally7788 <0702sally@gmail.com>
2025-08-12 16:58:53 +0900

    데코레이터 다시

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -16,7 +16,7 @@ PATH_TO_PYTHON_BINARY = sys.executable
 
 
 with DAG(
-    dag_id="dags_python_task_operator",
+    dag_id="dags_python_task_decorator",
     schedule="0 2 * * 1",
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     catchup=False,
